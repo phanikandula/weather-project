a78447db285f0989e6bd8f48549d87d44d421a61
Phani Kandula <phani.kandula@gmail.com>
2020-09-28 11:52:07 -0500

    Update test

--- projects/frontend/src/App.test.js
+++ projects/frontend/src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('zip based search', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/zip/i);
   expect(linkElement).toBeInTheDocument();
 });
